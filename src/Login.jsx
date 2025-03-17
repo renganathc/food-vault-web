e4848352d3e34ea4f6f0659d3e4e6ccc75d4dc81
Renganath Chokkalingam <rengnath1234@gmail.com>
2025-03-17 13:03:30 +0530

    Set login to appear after 2 sec

--- src/Login.jsx
+++ src/Login.jsx
@@ -8,19 +8,11 @@ function Login() {
         await signInWithGoogle();
     }
 
-    const [vis, setVis] = useState(false);
-
-    useEffect(() => {
-      setTimeout(() => {
-        setVis(true);
-      }, 1800);
-    }, []);
-
   return (
     <div>
         <img src='food.png' style={{width:"45vw"}}/>
         <h2 style={{marginTop:"-5vw", fontSize:27}}>Food Vault</h2>
-        <button className='login' onClick={handleSignIn} style={{display : vis ? "" : "none"}}>Log In with Student ID</button>
+        <button className='login' onClick={handleSignIn}>Log In with Student ID</button>
     </div>
   )
 }
